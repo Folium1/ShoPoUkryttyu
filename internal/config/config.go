34d2ab62674765e9514aae684363345399fd292a
Folium <folium@123.local>
2023-10-22 19:28:48 +0300

    Refactored Config struct

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,24 +9,31 @@ import (
 )
 
 type Config struct {
-	Env       string     `yaml:"env" env-default:"local"`
-	MongoPath string     `yaml:"mongo_path" env-default:"mongodb://localhost:27017" env-required:"true"`
-	MongoDB   string     `yaml:"mongo_db" env-default:"shelter" env-required:"true"`
-	HTTP      HTTPConfig `yaml:"http_server" env-required:"true"`
+	Env     string        `yaml:"env" env-default:"local"`
+	MongoDB MongoDBConfig `yaml:"mongo_db" env-required:"true"`
+	HTTP    HTTPConfig    `yaml:"http_server" env-required:"true"`
 }
 
 type HTTPConfig struct {
 	Port         string `yaml:"port" env-default:"8080" env-required:"true"`
-	Timeout      int    `yaml:"timeout" env-default:"5" env-required:"true"`
-	IddleTimeout int    `yaml:"iddle_timeout" env-default:"10" env-required:"true"`
+	Timeout      int    `yaml:"timeout" env-default:"5s" env-required:"true"`
+	IddleTimeout int    `yaml:"iddle_timeout" env-default:"10s" env-required:"true"`
+}
+
+type MongoDBConfig struct {
+	MongoPath          string `yaml:"mongo_path" env-default:"mongodb://localhost:27017" env-required:"true"`
+	DB                 string `yaml:"db" env-default:"shelter" env-required:"true"`
+	UsersCollection    string `yaml:"users_collection" env-default:"users" env-required:"true"`
+	SheltersCollection string `yaml:"shelters_collection" env-default:"shelters" env-required:"true"`
 }
 
 func NewConfig() Config {
-	cfg := Config{}
+	var cfg Config
 	err := cleanenv.ReadConfig("internal/config/config.yaml", &cfg)
 	if err != nil {
 		log.Fatalf("Configuration cannot be read: %v", err)
 	}
+
 	return cfg
 }
 
