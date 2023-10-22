5af52cee5eb2d6078f1c285b85b8ca41662fa1cf
Folium <vampirowitch.vampir@gmail.com>
2023-10-22 19:44:37 +0300

    Created basic users' model

---
+++ internal/models/user.go
@@ -0,0 +1,10 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type User struct {
+	ID       primitive.ObjectID `json:"id" bson:"_id"`
+	Name     string             `json:"name" bson:"name"`
+	Email    string             `json:"email" bson:"email,unique"`
+	Password string             `json:"password" bson:"password"`
+}
